09dfe08437b875d4d44854f4ec5f6acd2e5ba670
mk <mkpersonalpc@gmail.com>
2026-04-12 10:24:59 +0900

    Hello Worldを表示するだけのSpringプロジェクト

---
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -0,0 +1,20 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.example.demo.service.HelloService;
+
+@Controller
+public class HelloController {
+	@Autowired
+	HelloService helloService;
+	
+	@GetMapping("/hello")
+	public String hello() {
+		int num = helloService.calc(10, 20);
+		System.out.println(num);
+		return "hello";
+	}
+}
---
+++ src/test/java/com/example/demo/service/HelloServiceTest.java
@@ -0,0 +1,20 @@
+package com.example.demo.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloServiceTest {
+	
+	@Autowired
+	HelloService helloService;
+
+	@Test
+	void test() {
+		assertEquals(30, helloService.calc(10, 20));
+	}
+
+}
