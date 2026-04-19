eafe02b3c728087d3c4efe502d1452569a5b7320
mk <mkpersonalpc@gmail.com>
2026-04-19 09:42:32 +0900

    JUnitのテストを失敗させてみる

--- src/test/java/com/example/demo/service/HelloServiceTest.java
+++ src/test/java/com/example/demo/service/HelloServiceTest.java
@@ -1,7 +1,6 @@
 package com.example.demo.service;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -14,7 +13,7 @@ class HelloServiceTest {
 
 	@Test
 	void test() {
-		assertEquals(30, helloService.calc(10, 20));
+		assertEquals(31, helloService.calc(10, 20));
 	}
 
 }
